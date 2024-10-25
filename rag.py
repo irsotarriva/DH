6cc5d12d920e4430061df46a5c99063d5736049a
Isai Sotarriva <irsotarriva@gmail.com>
2024-10-25 17:28:30 +0900

    fix

--- rag.py
+++ rag.py
@@ -14,6 +14,7 @@ import subprocess
 import contextlib
 import logging
 from enum import Enum
+from UI import kaggle_login as kl
 log = logging.getLogger(__name__) # Set up logging
 #Try to import the necessary libraries. If they are not found, suggest the user how to install them.
 try:
